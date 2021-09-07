a34089d70d3783bcb6420a94e6bf477374a8fdf6
SincerelyFaust <hedesmarin@gmail.com>
2021-09-07 18:01:05 +0200

    Add forgotten files

---
+++ src/client.ts
@@ -0,0 +1,36 @@
+import "reflect-metadata";
+import path from "path";
+import { Intents, Interaction, Message } from "discord.js";
+import { Client } from "discordx";
+
+require('dotenv').config()
+
+const client = new Client({
+  prefix: "!",
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+  classes: [
+    path.join(__dirname, "commands", "**/*.{ts,js}"),
+    path.join(__dirname, "events", "**/*.{ts,js}"),
+  ],
+  botGuilds: [process.env.GUILD_ID ?? ""],
+  silent: true,
+});
+
+client.on("ready", () => {
+  client.initApplicationCommands({ log: { forGuild: true, forGlobal: true } });
+});
+
+client.on("interactionCreate", (interaction: Interaction) => {
+  client.executeInteraction(interaction);
+});
+
+client.on("messageCreate", (message: Message) => {
+  client.executeCommand(message);
+});
+
+client.login(process.env.BOT_TOKEN ?? ""); // provide your bot token
